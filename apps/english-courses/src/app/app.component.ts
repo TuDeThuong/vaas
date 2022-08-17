0cad985fdba501fe20ef76f847e2ddf74a9b4c11
AnhTu <nguyenanhtu1604@gmail.com>
2022-08-17 23:34:33 +0200

    initial commit

---
+++ apps/english-courses/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vaas-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'english-courses';
+}
